fe052f8f849562f518e38e12aa6431d88d960f1b
MargaritaKremen <53299362+MargaritaKremen@users.noreply.github.com>
2022-10-25 14:04:13 +0100

    add link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App">            
       Weather App
+      <footer>
+        This progect was created by Margarita Kozlova and is open-sourced {" "}        
+      <a href="https://github.com/MargaritaKremen/my-wheather-app-react"       
+      target="_blank" rel="noopener noreferrer"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
